dcfd4d990b1ca198f93250bca71a715f4442b0b3
Sergei_Nushchik <biz.nushchik@gmail.com>
2021-03-28 16:37:28 +0300

    Sekond Commit@

--- MyJavaProgram.java
+++ MyJavaProgram.java
@@ -1,7 +1,7 @@
 public class MyJavaProgram{
 	public static void main(String[] arrr){
 		for(String value : arrr){		
-		System.out.println("Hello" +value+ "!");
+		System.out.println("Hello" !!!!!! +value+ "!");
 		}
 	}
 }
